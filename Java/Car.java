ca09efb97d250f3071331726ba7ac2c604c5530d
edgarHdzDz <87505134+edgarHdzDz@users.noreply.github.com>
2022-02-26 20:54:31 -0600

    Creacion de objetos en Java y Python

--- Java/Car.java
+++ Java/Car.java
@@ -5,4 +5,10 @@ public class Car {
     String license;
     String driver;
     Integer passenger;
+
+    //  Metodo para traer los datos
+
+    void printDataCar() {
+        System.out.println("License: " + license + " Driver: " + driver);
+    }
 }
